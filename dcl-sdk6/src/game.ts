7bcb62566bb2b7055dfdbf8660972a7af5994b4a
Unknower <dev@unknower.com>
2023-08-04 08:03:54 -0400

    debugging a certain set of code

--- dcl-sdk6/src/game.ts
+++ dcl-sdk6/src/game.ts
@@ -1,2 +1,12 @@
-import { VLM } from "vlm-dcl";
-VLM.init();
+//
+// IMPORTANT : 
+// - include `noLib: false` to your tsconfig.json file, under "compilerOptions"
+//
+///<reference lib="es2015.symbol" />
+///<reference lib="es2015.symbol.wellknown" />
+///<reference lib="es2015.collection" />
+///<reference lib="es2015.iterable" />
+
+import { Client } from "colyseus.js";
+
+const client = new Client("wss://api.vlm.gg");
