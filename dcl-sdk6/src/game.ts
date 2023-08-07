1498b569b265859b1c6176fe7bd28f71f02532bf
Unknower <dev@unknower.com>
2023-08-07 19:25:38 -0400

    chore: clean up demo scene

--- dcl-sdk6/src/game.ts
+++ dcl-sdk6/src/game.ts
@@ -1,12 +1,6 @@
-//
-// IMPORTANT : 
-// - include `noLib: false` to your tsconfig.json file, under "compilerOptions"
-//
 ///<reference lib="es2015.symbol" />
 ///<reference lib="es2015.symbol.wellknown" />
 ///<reference lib="es2015.collection" />
 ///<reference lib="es2015.iterable" />
-
-import { Client } from "colyseus.js";
-
-const client = new Client("wss://api.vlm.gg");
+import { VLM } from "vlm-dcl";
+VLM.init({ env: "dev" });
